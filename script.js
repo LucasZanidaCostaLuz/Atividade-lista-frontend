aa1d82af313e38c1cc74b81f7e07e61a4cceac47
LucasZaniDaCostaLuz <zanin0503@gmail.com>
2025-02-06 11:52:53 -0300

    feat: start of project

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("task-giver").addEventListener('submit', function(event){
+    event.preventDefault();
+    const taskInput =document.getElementById("task-added");
+    const taskText = taskInput.ariaValueMax.trim();
+
+    if(taskText !== ""){
+        const taskList = 
+    }
+})
