99da4ca1ad3bda1639b26edf2b08ace959a3da2b
LucasZaniDaCostaLuz <zanin0503@gmail.com>
2025-02-06 15:34:54 -0300

    feat: finish project

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-document.getElementById("task-giver").addEventListener('submit', function(event){
+document.getElementById('task-giver').addEventListener('submit', function(event) {
     event.preventDefault();
-    const taskInput =document.getElementById("task-added");
-    const taskText = taskInput.ariaValueMax.trim();
-
-    if(taskText !== ""){
-        const taskList = 
+    
+    const taskInput = document.getElementById('task-added');
+    const taskText = taskInput.value.trim();
+    
+    if (taskText !== '') {
+        const taskList = document.getElementById('task-list');
+        const newTask = document.createElement('li');
+        newTask.textContent = taskText;
+        
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remover';
+        removeButton.style.marginLeft = '1vh';
+        removeButton.addEventListener('click', function() {
+            taskList.removeChild(newTask);
+        });
+        
+        newTask.appendChild(removeButton);
+        taskList.appendChild(newTask);
+        taskInput.value = '';
     }
-})
+});
